7fc25486fa4843258926b7f4fdbd969f589aab58
José Vitor <jvvmartos@hotmail.com>
2024-04-11 16:06:55 -0300

    Emissive Light Throughput System
    Implementation of a different light system based on throughtput instead of pure diffuse light
    
    This model still raw and lacks reflection and other features

--- Shadertoy/bufferA.glsl
+++ Shadertoy/bufferA.glsl
@@ -7,7 +7,9 @@ struct Material {
     vec4 albedo;
     float roughness;
     float metalicness;
-    bool emissive;
+    
+    vec3 emissiveColor;
+    float emissivePower;
 };
 
 struct Sphere {
@@ -27,8 +29,11 @@ struct RenderData {
 };
 
 #define MAX_DIST 100.0
-#define MAX_LIGHT_BOUNCES 5
-vec3 lightPos = vec3(0.0, 2.0, 0.0); //normalize(vec3(1.0));
+#define MAX_LIGHT_BOUNCES 16
+#define MIN_TRACE_DIST 0.001
+#define NORMAL_OFFSET 0.001
+
+vec3 lightPos = (vec3(2.0, 3.0, 3.0))*1.0;
 
 
 
@@ -50,20 +55,23 @@ float iSphere(in Ray ray, in Sphere sph)
 	return t;
 }
 
-bool miss(float hit) { return hit <= 0.0; }
+bool miss(float hit) { return hit <= MIN_TRACE_DIST || hit > MAX_DIST; }
 
 Scene world(Ray ray) {
     Scene scene;
     scene.d = MAX_DIST;
     
-    Material m1 = Material(vec4(0.1, 0.7, 0.9, 1.0), 1.0, 0.0, false); // sin(iTime)*0.5 + 0.5
-    Material m2 = Material(vec4(0.9, 0.4, 0.1, 1.0), 1.0, 0.5, false);
-    Material lm = Material(vec4(1.0), 0.0, 0.0, true);
+    Material m1 = Material(vec4(0.1, 0.7, 0.9, 1.0), 1.0, 0.0, vec3(0.0), 0.0); // sin(iTime)*0.5 + 0.5
+    Material m2 = Material(vec4(0.9, 0.4, 0.1, 1.0), 1.0, 0.5, vec3(0.0), 0.0);
+    Material refM = Material(vec4(0.8), 0.05, 0.0, vec3(0.0), 0.0);
+    
+    Material wlm = Material(vec4(0.0), 1.0, 0.0, vec3(1.0), 20.0);
+    Material lm = Material(vec4(1.0), 1.0, 0.0, vec3(0.9, 0.5, 0.1), 20.0);
     
     Sphere[] sphs = Sphere[] (
         Sphere(vec3(0.0), 0.85, m1),
-        Sphere(vec3(0.0, 0.91, 0.0), 0.05, m2),
-        Sphere(lightPos, 0.1, lm) // lightSrc
+        Sphere(vec3(0.0, 0.9, 0.0), 0.05, m2),
+        Sphere(lightPos, 2.0, lm) // lightSrc
     );
     
     for(int i = 0; i < 3; i++) {
@@ -91,95 +99,77 @@ vec4 skyColor(vec2 uv) {
     float green = 0.25;
     float blue = 0.5;
     
-    vec4 color = vec4(red, green, blue, 1.0) * 1.5; 
+    vec4 color = vec4(red, green, blue, 1.0) * 2.0; 
     color = vec4(0.0);
-    return color; //vec4(0.0);
+    return color;
+}
+
+uint seed = 0u;
+vec2 fragCoords = vec2(0.0);
+void genSeed() {
+    seed = uint(uint(uint(fragCoords.x) * uint(1973) + uint(fragCoords.y) * uint(9277) + uint(iFrame) * uint(26699)) | uint(1));
 }
 
 RenderData worldRender(vec2 uv, Ray ray) {
-    vec4 color = vec4(0.0);
     
     //worldLight();
     
-    Scene scene = world(ray);
-    //if(scene.d >= MAX_DIST) return RenderData(scene, skyColor(uv));
-    // Because normals only work for (a,b)=(0,0), gotta shift ray.origin
-    //ray.origin -= closestHit;
-
-    float decay = 1.0;
-
-    if (!scene.closestHit.mat.emissive) {
+    Scene scene;
     
-        for(int i = 0; i < MAX_LIGHT_BOUNCES; i++) {
-            scene = world(ray);
-            if(scene.d >= MAX_DIST) { color += skyColor(uv) * decay; break; }
-            vec3 closestHit = scene.closestHit.pos;
-            
-            ray.origin -= closestHit;
-            
-            vec3 hitPoint = ray.origin + (ray.dir*scene.d);
-            vec3 normals = normalize(hitPoint);
-            
-            
-            // Shade/Color
-            vec3 lightDir = normalize(lightPos);
-            float shadedScene = dot(normals, lightDir);
-            //shadedScene+=1.0;
-            //shadedScene/=2.0; // Remap normals from [-1 1] to [0 1]
+    vec3 color = vec3(0.0);
+    vec3 light = vec3(0.0);
+    vec3 throughput = vec3(1.0);
 
-            float al = 0.25;
-
-            if(i == MAX_LIGHT_BOUNCES-1 ) { color *= 0.1; break; }
-            
-            vec4 ambientShadedScene = vec4(max(shadedScene, 0.0));
-            vec4 hitColor = scene.closestHit.mat.albedo;
-            color += ambientShadedScene * hitColor * decay;
-            //color = vec4(ray.dir, 1.0);
-            //color = vec4(vec3(scene.d), 0.0);
-            //color = vec4(hitPoint, 0.0);
-            //
-            
-            // Trace
-            ray.origin = hitPoint + closestHit + normals * 0.001;
-            
-            float aspect = iResolution.x / iResolution.y;
-            vec2 aspectlessUV = uv;
-            aspectlessUV.x /= aspect;
-            vec2 fc = (aspectlessUV + 1.0) / 2.0 * iResolution.xy;
-            uint seed = uint(uint(uint(fc.x) * uint(1973) + uint(fc.y) * uint(9277) + uint(iFrame) * uint(26699)) | uint(1));
-            //float seed = 1.0;
-            
-            
-            vec3 randomSpread = RandomUnitVector(seed); //vec3( randomRange(uv*iTime, -0.5, 0.5), randomRange(uv*iTime, -0.5, 0.5), randomRange(uv*iTime, -0.5, 0.5) );
-            vec3 normalShift = scene.closestHit.mat.roughness *  randomSpread;
-            ray.dir = normalize(reflect(ray.dir, normals + normalShift));
-            
-            
-            decay *= 0.5;
-        }
+    for(int i = 0; i < 2; i++) {
+        scene = world(ray);
+        if(scene.d >= MAX_DIST) { light += skyColor(uv).rgb * throughput; break; }
+        vec3 closestHit = scene.closestHit.pos;
         
-        return RenderData(scene, color);
+        //if(i == MAX_LIGHT_BOUNCES-1 ) { light *= 0.0; break; }
+
+        //ray.origin -= closestHit;
+        vec3 hitPoint = ray.origin - closestHit + (ray.dir*scene.d);
+        
+        vec3 normals = normalize(hitPoint);
+        // Remap normals from [-1 1] to [0 1]
+        
+
+        vec4 hitColor = scene.closestHit.mat.albedo;
+        
+        throughput *= hitColor.rgb;
+        light += scene.closestHit.mat.emissivePower * (throughput + scene.closestHit.mat.emissiveColor*0.25);
+        
+        //light += max(dot(normalize(lightPos-closestHit), normals) * scene.closestHit.mat.albedo.rgb, 0.0);
+
+        
+        color = normals;
+        
+        // Trace
+        ray.origin = hitPoint + closestHit + normals*NORMAL_OFFSET;
+
+        genSeed();
+
+        vec3 randomSpread = RandomUnitVector(seed);
+        vec3 normalShift = scene.closestHit.mat.roughness * randomSpread;
+        ray.dir = normalize(ray.dir + normalShift); //normalize(reflect(ray.dir, normals + normalShift));
     }
-    else return RenderData(scene, vec4(scene.d));
+        
+    //light += throughput/float(MAX_LIGHT_BOUNCES/2);
+    return RenderData(scene, vec4(light, 1.0));
 }
 
 mat2 rotate(float angle) {
     return mat2(cos(angle), -sin(angle), sin(angle), cos(angle));
 }
 
-vec2 gMouse = vec2(0.0);
 bool mouseUpdate(inout vec3 camera) {
     vec2 mouse = iMouse.xy / iResolution.xy * 2.0;
     
     //camera.yz *= rotate(mouse.y * 3.14);
-    //camera.yz *= rotate(mouse.y * 3.14 * 1.5);
-    //camera.z -= cos(mouse.y);
     camera.xz *= rotate(mouse.x * 3.14 * 1.5);
     //camera.xz *= rotate(iMouse.x * 20.0 / iChannelResolution[0].x);
     //camera.y = iMouse.y * 5.0 / iChannelResolution[0].y - 1.0;
-    
-    bool changed = gMouse != mouse;
-    gMouse = mouse;
+
     return false;
 }
 
@@ -190,15 +180,15 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
     float aspect = iResolution.x / iResolution.y;
     uv.x *= aspect;
     
-    vec3 ro = vec3(0.0, 0.9, -0.1);
+    vec3 ro = vec3(0.0, 0.9*1.0, -0.1);
     vec3 rd = normalize(vec3(uv, 1.0));
     //rd.xz *= rotation(-sin(iTime)*3.14/2.0);//
-    bool mouseUpdated = false;
-    mouseUpdated = mouseUpdate(ro);
+    
+    mouseUpdate(ro);
     mouseUpdate(rd);
-    if (mouseUpdated) { fragColor = vec4(0.0); return; }
     
     Ray ray = Ray(ro, rd);
+    fragCoords = fragCoord;
     
     vec4 finalRender = worldRender(uv, ray).color;
     vec4 lastFrame = texture(iChannel0, fragCoord/iResolution.xy);
